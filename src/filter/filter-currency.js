e3652413eae05647fd3c3fcf01d0417f3dfc605d
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-09 10:03:05 +0800

    init page

--- src/filter/filter-currency.js
+++
@@ -1,4 +0,0 @@
-export default (input) => {
-  let val = (input / 1).toFixed(0).replace('.', ',')
-  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
--- src/filter/filter-limit.js
+++
@@ -1,9 +0,0 @@
-export default (input, limit, tail) => {
-  if (tail === undefined) {
-    tail = ''
-  }
-  if (input === undefined) {
-    input = ''
-  }
-  return input.toString().substring(0, limit) + tail
-}
